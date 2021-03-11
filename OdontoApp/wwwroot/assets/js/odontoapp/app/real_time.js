ded965696a7506ea257be67ecf672b63f44a1a9c
uanderson lima <uandersonlimacs@gmail.com>
2021-03-10 22:15:52 -0300

    features: funções JS para o chat consumindo a API

--- OdontoApp/wwwroot/assets/js/odontoapp/app/real_time.js
+++ OdontoApp/wwwroot/assets/js/odontoapp/app/real_time.js
@@ -1,39 +1,51 @@
+
+//Configura conexão com o signalR
 let connectionMsg = new signalR.HubConnectionBuilder()
     .withUrl("/messagehubservice")
     .configureLogging(signalR.LogLevel.Information)
     .build();
-connectionMsg.start().then(() => connectionMsg.on("reportnewmessagesasync", (msg, cdc) => console.log(msg, cdc)));
+
+
+    //Starta conexão
+    connectionMsg.start().then(() => connectionMsg.on("reportnewmessagesasync", (msg, cdc) => {
+    appendNewMessage(cdc);
+}
+));
+
+
 connectionMsg.on("UserIsConnectedAsync", msg => console.log(msg))
 connectionMsg.on("UserIsDisconnectedAsync", msg => console.log(msg))
 
+inputIdUser = document.querySelector('input[id="user"]');
 
-
-
-
-let request = new Request("/api/messages", {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        description: "salve meu compatriota",
-        isDeleted: false,
-        messagecode: null,
-        receiverUser: null,
-        receiverUserId: "6e713cc6-07f7-44e9-a559-3fdc3671d91b",
-        senderUser: null,
-        senderUserId: "",
-        timeReceived: "",
-        timeSent: "",
-        viewedTime: "",
-    })
-}
-)
-
+//Função teste para enviar mensagem fixa; 
 function teste() {
+    let request = new Request("/api/messages", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            description: "salve meu compatriota",
+            isDeleted: false,
+            messagecode: null,
+            receiverUser: null,
+            receiverUserId: inputIdUser.value,
+            senderUser: null,
+            senderUserId: "",
+            timeReceived: "",
+            timeSent: "",
+            viewedTime: "",
+        })
+    }
+    )
     fetch(request)
-        .then(response => console.log(response.json()))
+        .then(response => {
+            if (response.status == 200) {
+                console.log('ok');
+            }
+        })
         .catch(error => console.error('Unable to update item.', error));
 }
 
--- OdontoApp/wwwroot/assets/js/odontoapp/message/message_index.js
+++ OdontoApp/wwwroot/assets/js/odontoapp/message/message_index.js
@@ -1,26 +1,52 @@
 const chat = {
     boxMessage: document.querySelector('div.chat'),
-    ballonMessage: document.querySelector('div.balao2'),
+    ballonMessage1: document.querySelector('div.balao1'),
+    ballonMessage2: document.querySelector('div.balao2'),
     inputMessage: document.querySelector('input[id="mensagem"]'),
     sendButton: document.querySelector('button#butaozinho')
 }
 
 const scrollToBottom = () => {
     let shouldScroll = chat.boxMessage.scrollTop + chat.boxMessage.clientHeight === chat.boxMessage.scrollHeight;
-    if(!shouldScroll)
+    if (!shouldScroll)
         chat.boxMessage.scrollTop = chat.boxMessage.scrollHeight;
 }
 
+//MENSAGEM ENVIADOR
+//melhorar com a resposta do Vzinho
 const appendMessage = () => {
-    teste();
-    const newMessage = chat.ballonMessage.parentNode.parentNode.cloneNode(true);
-    newMessage.querySelector('p.m-3').textContent = chat.inputMessage.value;
+    if (!chat.inputMessage.value) {
+    }
+    else {
+        const newMessage = chat.ballonMessage2.parentNode.parentNode.cloneNode(true);
+        newMessage.querySelector('p.m-3').textContent = chat.inputMessage.value;
+        chat.boxMessage.appendChild(newMessage);
+        chat.inputMessage.value = "";
+        scrollToBottom();
+        teste();
+    }
+}
+
+//MESSAGEM RECEBEDOR
+const appendNewMessage = (msg = null) => {
+    const newMessage = chat.ballonMessage1.parentNode.parentNode.cloneNode(true);
+    newMessage.querySelector('p.m-3').textContent = msg.description + msg.timeSent;
     chat.boxMessage.appendChild(newMessage);
-    chat.inputMessage.value = "";
     scrollToBottom();
 }
 
+
 chat.sendButton.addEventListener("click", appendMessage
+
+);
+chat.inputMessage.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        event.preventDefault();
+        // Do more work
+        appendMessage();
+    }
+}
 );
 
 
